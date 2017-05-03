9ee7ecdd4ba5b7e804120810db3e59aaf92962ad
Christian Cambizaca <christianismypapichulo@gmail.com>
2017-05-03 22:29:02 +0000

    lit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # classRandomizerStarterCode
+Hello! 
--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
+var stu = ["Jessie Ann Navas", 
+           "Luis Dilone", 
+           "Kiara Acosta Correa", 
+           "Sammy Ech-Chouy", 
+           "Christian Cambizaca"
+           ];
 
+var te = ["Aaron", 
+          "Zack", 
+          "Justin", 
+          "Julia", 
+          "Alyxe"];
+
+ 
+
+var random = Math.floor(Math.random()*stu.length);
+
+
+$(studentButton).click(function() {
+    $(studentDisplay).append(stu[random], " " );
+    
+});
+
+$(teacherButton).click(function() {
+    $(teacherDisplay).append(te[random], " " );
+});
 
 
 });
